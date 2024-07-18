45e203a9b37668e7d9d3ed5fb0e2352b40995fde
vokashlob <58624355+vokashlob@users.noreply.github.com>
2024-07-18 09:59:53 +0300

    first commit

---
+++ base_structures.py
@@ -0,0 +1,15 @@
+#1st program
+
+print(9 ** 0.5 * 5)
+
+#2nd program
+
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3rd program
+
+print(1234 % 1000 // 10 + 5678 % 1000 // 10)
+
+#4th program
+
+print(int(13.42) == int(42.13 % 42 * 100) or int(13.42 % 13 * 100) == int(42.13))
