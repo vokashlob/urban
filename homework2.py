bf2e77ca9753fca3c85a5eabf437c3eca47aecc5
vokashlob <58624355+vokashlob@users.noreply.github.com>
2024-07-21 16:49:46 +0300

    first commit

--- homework2.py
+++ homework2.py
@@ -3,7 +3,9 @@ hours_spent = 1.5
 course_name = 'Python'
 average_time = hours_spent / homeworks_count
 
-print('Курс: ' + course_name + ', всего задач: ' + str(homeworks_count) + ', затрачено часов: ' + str(hours_spent) + ', среднее время выполнения: ' + str(average_time) + ' часов или ' + str(average_time * 60) + ' минут.')
+print('Курс:', course_name, ', всего задач:', homeworks_count, ', затрачено часов:', hours_spent, ', среднее время выполнения:', average_time, 'часа или', average_time * 60, 'минут.')
 
-# менее громоздкий вариант, не требующий, к тому же, приведения типов
-print(f'Курс: {course_name}, всего задач: {homeworks_count}, затрачено часов: {hours_spent}, среднее время выполнения: {average_time} часов или {average_time * 60} минут.')
+# Поскольку Python сам ставит пробелы после переменных, перечисленных в скобках функции print,
+# то, с точки зрения пунктуации, лучше отформатировать вывод через f-strings:
+
+print(f'Курс: {course_name}, всего задач: {homeworks_count}, затрачено часов: {hours_spent}, среднее время выполнения: {average_time} часа или {average_time * 60} минут.')
