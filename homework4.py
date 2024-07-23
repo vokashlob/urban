aac54501fc0c4890f9874aad5b00aec66396300e
vokashlob <58624355+vokashlob@users.noreply.github.com>
2024-07-23 07:48:24 +0300

    first commit

---
+++ homework4.py
@@ -0,0 +1,18 @@
+my_string = input('Введите произвольный текст: ')
+length = len(my_string)
+ending_digits_var0 = (2, 3, 4)
+ending_digits_var1 = (0, 5, 6, 7, 8, 9)
+
+if length % 10 in ending_digits_var0:
+    ending = 'а'
+elif length % 10 in ending_digits_var1:
+    ending = 'ов'
+else:
+    ending = ''
+
+print('Во введенной Вами строке', length, 'символ' + ending)
+print('Заглавные:', my_string.upper())
+print('Прописные:', my_string.lower())
+print('Без пробелов:', my_string.replace(' ', ''))
+print('Первый символ строки:', my_string[0])
+print('Последний символ строки', my_string[-1])
