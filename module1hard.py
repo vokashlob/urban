66c02481a0cd327bde5fc2d63312579621579106
vokashlob <58624355+vokashlob@users.noreply.github.com>
2024-07-25 09:25:19 +0300

    dict changes (second commit)

--- module1hard.py
+++ module1hard.py
@@ -4,6 +4,7 @@ grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 
 # преобразуем students в сортированный список
+# students = sorted(list(students))
 students = sorted(students)
 
 # вычисляем среднее для каждой группы оценок
