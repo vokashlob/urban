19f0501563895d11e016e9ce995174fe2deacff7
vokashlob <58624355+vokashlob@users.noreply.github.com>
2024-07-25 09:23:41 +0300

    dict changes (second commit)

--- module1hard.py
+++ module1hard.py
@@ -4,12 +4,13 @@ grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 
 # преобразуем students в сортированный список
-students = sorted(list(students))
+students = sorted(students)
 
 # вычисляем среднее для каждой группы оценок
 average_grade = [sum(i) / len(i) for i in grades]
 
 # генерируем и выводим на консоль словарь
-grades_dict = {k: v for k, v in zip(students, average_grade)}
+#grades_dict = {k: v for k, v in zip(students, average_grade)}
+grades_dict = dict(zip(students, average_grade))
 
 print(grades_dict)
