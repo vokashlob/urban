b794c2e88a3a8dfb9a3f577ac1e763128ac4bb4f
vokashlob <58624355+vokashlob@users.noreply.github.com>
2024-07-25 08:23:28 +0300

    second commit

--- homework4.py
+++ homework4.py
@@ -3,12 +3,14 @@ length = len(my_string)
 ending_digits_var0 = (2, 3, 4)
 ending_digits_var1 = (0, 5, 6, 7, 8, 9)
 
-if length % 10 in ending_digits_var0:
-    ending = 'а'
-elif length % 10 in ending_digits_var1:
-    ending = 'ов'
-else:
-    ending = ''
+# if length % 10 in ending_digits_var0:
+#     ending = 'а'
+# elif length % 10 in ending_digits_var1:
+#     ending = 'ов'
+# else:
+#     ending = ''
+
+ending = 'a' if length % 10 in ending_digits_var0 else ('ов' if length % 10 in ending_digits_var1 else "")
 
 print('Во введенной Вами строке', length, 'символ' + ending)
 print('Заглавные:', my_string.upper())
