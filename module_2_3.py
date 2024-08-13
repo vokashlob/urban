b97f952001ae45a87a2acdc7233f4f49078a56a6
vokashlob <58624355+vokashlob@users.noreply.github.com>
2024-08-13 11:16:37 +0300

    first commit

--- module_2_3.py
+++ module_2_3.py
@@ -2,13 +2,13 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 
 number = 0
-while True:
-    if my_list[number] == 0:
-        number += 1
-        continue
+while number != len(my_list):
+    if my_list[number] < 0:
+        break
     elif my_list[number] > 0:
         print(my_list[number])
         number += 1
         continue
     else:
-        break
+        number += 1
+        continue
