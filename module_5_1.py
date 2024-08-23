af51b43ca1bcc3e46b1339b81778080e4c7279e7
vokashlob <58624355+vokashlob@users.noreply.github.com>
2024-08-23 09:28:47 +0300

    first commit

---
+++ module_5_1.py
@@ -0,0 +1,23 @@
+
+# создание класса и методов внутри него
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors:
+            print('Такого этажа не сущестует!')
+        else:
+            for i in range(1, new_floor + 1):
+                print(i)
+
+
+# проверки
+
+h1 = House('Эдьбрус', 25)
+h2 = House('Машук', 2)
+h3 = House('Казбек', 10)
+h1.go_to(5)
+h2.go_to(10)
+h3.go_to(10)
