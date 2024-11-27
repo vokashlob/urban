da1fea404c58ce04c81230c9a591a935879c5b18
vokashlob <58624355+vokashlob@users.noreply.github.com>
2024-11-27 11:52:58 +0300

    first commit

--- module_8_2.py
+++ module_8_2.py
@@ -10,6 +10,7 @@ def personal_sum(numbers):
             incorrect_data += 1
     return result, incorrect_data
 
+
 def calculate_average(numbers):
     try:
         length = 0
@@ -24,7 +25,7 @@ def calculate_average(numbers):
         return None
 
 
-print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка перебирается, но каждый символ - строковый тип
-print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
-print(f'Результат 3: {calculate_average(567)}') # Передана не коллекция
-print(f'Результат 4: {calculate_average([42, 15, 36, 13])}') # Всё должно работать
+print(f'Результат 1: {calculate_average("1, 2, 3")}')  # Строка перебирается, но каждый символ - строковый тип
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')  # Учитываются только 1 и 3
+print(f'Результат 3: {calculate_average(567)}')  # Передана не коллекция
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')  # Всё должно работать
