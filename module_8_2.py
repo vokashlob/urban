1a0256b69f044d4d464001ddb3d66e8ea7355ecb
vokashlob <58624355+vokashlob@users.noreply.github.com>
2024-11-27 11:50:36 +0300

    first commit

---
+++ module_8_2.py
@@ -0,0 +1,30 @@
+
+def personal_sum(numbers):
+    result = 0
+    incorrect_data = 0
+    for num in numbers:
+        try:
+            result += num
+        except TypeError:
+            print(f'Некорректный тип данных для подсчетe суммы - {num}')
+            incorrect_data += 1
+    return result, incorrect_data
+
+def calculate_average(numbers):
+    try:
+        length = 0
+        for i in numbers:
+            if isinstance(i, (int, float)):
+                length += 1
+        return personal_sum(numbers)[0] / length
+    except ZeroDivisionError:
+        return 0
+    except TypeError:
+        print(f'В numbers записан некорректный тип данных')
+        return None
+
+
+print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка перебирается, но каждый символ - строковый тип
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
+print(f'Результат 3: {calculate_average(567)}') # Передана не коллекция
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}') # Всё должно работать
