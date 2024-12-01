c3ab0f6a70bfeb62324ed3afae94ee2178b1cfac
vokashlob <58624355+vokashlob@users.noreply.github.com>
2024-12-01 18:59:09 +0300

    first commit

---
+++ module_9_4.py
@@ -0,0 +1,32 @@
+from random import choice
+
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+
+print(list(map(lambda x, y: x == y, first, second)))
+
+
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open(file_name, 'w', encoding='utf-8') as file:
+            for string in data_set:
+                file.write(str(string) + '\n')
+    return write_everything
+
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+
+class MysticBall:
+    def __init__(self, *words):
+        self.words = words
+
+    def __call__(self):
+        return choice(self.words)
+
+
+first_ball = MysticBall('Несомненно', 'Ни в коем случае', 'Да кто его знает')
+print(first_ball())
+print(first_ball())
+print(first_ball())
