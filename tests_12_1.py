186c751790cddaf028746751bd846ac0e8222927
vokashlob <58624355+vokashlob@users.noreply.github.com>
2024-12-10 14:38:59 +0300

    first commit

--- tests_12_1.py
+++ tests_12_1.py
@@ -23,5 +23,5 @@ class RunnerTest(unittest.TestCase):
             r2.walk()
         self.assertNotEqual(r1.distance, r2.distance)
 
-    if __name__ == '__main__':
+if __name__ == '__main__':
         unittest.main()
