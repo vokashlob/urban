5391db7b4b505a10089e907d0264cc2ae8c1cc65
vokashlob <58624355+vokashlob@users.noreply.github.com>
2024-12-10 14:16:19 +0300

    first commit

---
+++ tests_12_1.py
@@ -0,0 +1,24 @@
+import runner
+import unittest
+
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        r1 = runner.Runner('Усейн Болт')
+        for _ in range(10):
+            r1.walk()
+        self.assertEqual(r1.distance, 50)
+
+    def test_run(self):
+        r2 = runner.Runner('Джошуа Чептегеи')
+        for _ in range(10):
+            r2.run()
+        self.assertEqual(r2.distance, 100)
+
+    def test_challenge(self):
+        r1 = runner.Runner('Усейн Болт')
+        r2 = runner.Runner('Джошуа Чептегеи')
+        for _ in range(10):
+            r1.run()
+            r2.walk()
+        self.assertNotEqual(r1.distance, r2.distance)
