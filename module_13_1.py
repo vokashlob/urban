98097bb927adc15ea52613652cba5b9145d52862
vokashlob <58624355+vokashlob@users.noreply.github.com>
2024-12-14 19:43:16 +0300

    first commit

---
+++ module_13_1.py
@@ -0,0 +1,20 @@
+import asyncio
+
+
+async def start_strongman(name, power):
+    print(f'Силач {name} начал соревнования')
+    for _ in range(5):
+        await asyncio.sleep(1 / power)
+        print(f'Силач {name} поднял {_ + 1} шар')
+    print(f'Силач {name} закончил соревнования')
+
+
+async def start_tournament():
+    task1 = asyncio.create_task(start_strongman('Pasha', 3))
+    task2 = asyncio.create_task(start_strongman('Denis', 4))
+    task3 = asyncio.create_task(start_strongman('Apollon', 5))
+    await task1
+    await task2
+    await task3
+
+asyncio.run(start_tournament())
