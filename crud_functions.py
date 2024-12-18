4c8162e781565ae4d71d9868801817c0ba80bea9
vokashlob <58624355+vokashlob@users.noreply.github.com>
2024-12-18 16:31:29 +0300

    first commit

--- crud_functions.py
+++ crud_functions.py
@@ -3,14 +3,27 @@ import sqlite3
 connection = sqlite3.connect('products.db')
 cursor = connection.cursor()
 
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS Products(
-id INT PRIMARY KEY,
-title TEXT NOT NULL,
-description TEXT,
-price INT NOT NULL
-);
-''')
+
+def initiate_db():
+
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Products(
+    id INT PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    price INT NOT NULL
+    );
+    ''')
+
+    cursor.execute('''
+    CREATE TABLE IF NO EXISTS Users(
+    id INT PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INT NOT NULL,
+    balance INT NOT NULL
+    );
+    ''')
 
 
 def add_product(id, title, description, price):
@@ -23,6 +36,10 @@ def get_all_products():
     products = cursor.fetchall()
     connection.commit()
     return products
+
+def add_user(username, email, age):
+    cursor.execute(f'INSERT INTO Users VALUES("{username}", "{email}", "{age}", "1000")')
+    connection.commit()
 
 
 # add_product(1, 'Постельное белье 2 спальное Веселина, Бербари, бязь, наволочки 50х70, 100% хлопок',
